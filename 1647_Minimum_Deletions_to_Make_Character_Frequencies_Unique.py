78e48980ab916892b768e030dbfeeeab5b74800f
Hammurabi1750BC <86497776+Hammurabi1750BC@users.noreply.github.com>
2021-06-25 16:29:29 -0700

    Create 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.py

---
+++ 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        min_dels = 0
+        s = Counter(s)
+        tallies = set()
+        d_same_freq = defaultdict(lambda: 1)  # if you have a match, start at 2
+        
+        for char in s.keys():
+            tally = s[char]
+            if tally in tallies:
+                d_same_freq[tally] += 1
+            tallies.add(tally)
+        
+        for tally in d_same_freq.keys():
+            instances2move = d_same_freq[tally]
+            while instances2move > 1:
+                next_available = max(set(range(tally)).difference(tallies))
+                min_dels += tally - next_available
+                if next_available > 0:
+                    tallies.add(next_available)
+                instances2move -= 1
+                    
+        return min_dels
+    
