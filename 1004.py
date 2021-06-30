f2847a9e3a8b4b68f3248f77a497d6454e416348
Hammurabi1750BC <86497776+Hammurabi1750BC@users.noreply.github.com>
2021-06-30 08:02:57 -0700

    Create 1004.py

--- 1004.py
+++ 1004.py
@@ -1,6 +1,26 @@
 # 1004 Max Consecutive Ones III
 
 class Solution:
+    
+    # 
+        l = 0
+        
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                k -= 1
+                
+            if k < 0:
+                if nums[l] == 0:
+                    k += 1
+                l += 1
+            
+        
+        return r-l+1
+    
+    
+    
+    # first attempt
+    
     def longestOnes(self, nums: List[int], k: int) -> int:
         if k >= len(nums):
             return len(nums)
