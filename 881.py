73703eb108d9d7e95966343b5dc9b7aec59955af
Hammurabi1750BC <86497776+Hammurabi1750BC@users.noreply.github.com>
2021-07-23 08:22:42 -0700

    Create 881.py

---
+++ 881.py
@@ -0,0 +1,15 @@
+# 881	Boats to Save People
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        
+        boats, left, right = 0, 0, len(people) - 1
+        
+        while left <= right:
+            if people[right] + people[left] <= limit:
+                left += 1
+            boats += 1
+            right -= 1        
+        
+        return boats
